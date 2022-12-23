1c01964aab19d58a13cdd01b2ab8baecc00851fb
Alexei Fedotkin <alexei.fedotkin@gmail.com>
2022-12-23 14:12:06 +0300

    Changed to .NET 7.0 and used the new program style

--- trey-nash/Program.cs
+++ trey-nash/Program.cs
@@ -1,27 +1,24 @@
 ﻿using Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts;
-class Program
+//TODO
+//namespace Fedotkin.Dotnet.TreyNash.General;
+
+/// <summary>
+/// Main entry point of the program.
+/// </summary>
+Console.Write("Enter chapter number (1-17): ");
+int chapterNo = 0;
+while (chapterNo == 0)
 {
-    /// <summary>
-    /// Main entry point of the program.
-    /// </summary>
-    static void Main()
-    {
-        Console.Write("Enter chapter number (1-17): ");
-        int chapterNo = 0;
-        while (chapterNo == 0)
-        {
-            try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
-            catch { chapterNo = 0; }
-        }
-        // Select book chapter and run the demo
-        switch (chapterNo)
-        {
-            case 5:
-                Chapter5.Run();
-                break;
-            default:
-                Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
-                break;
-        }
-    }
+    try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
+    catch { chapterNo = 0; }
+}
+// Select book chapter and run the demo
+switch (chapterNo)
+{
+    case 5:
+        Chapter5.Run();
+        break;
+    default:
+        Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
+        break;
 }
