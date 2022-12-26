ae129d0714c62b5045171a0420046e2505d22618
Alexei Fedotkin <110036665+fedotkin@users.noreply.github.com>
2022-12-26 08:29:55 +0300

    Revert "#25 Upgrade to .NET 7.0 (#28)" (#29)
    This reverts commit 81f6883c41d5b0c904e8be2a6043771389894cf3.

--- trey-nash/5-interfaces-and-contracts/Chapter5.cs
+++ trey-nash/5-interfaces-and-contracts/Chapter5.cs
@@ -1,15 +1,16 @@
-﻿namespace Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts;
-
-static class Chapter5
+﻿namespace Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts
 {
-    /// <summary>
-    /// Runs the demo of <see cref="Exercise1"/>.
-    /// </summary>
-    public static void Run()
+    static class Chapter5
     {
-        Exercise1.ClassA obj = new Exercise1.ClassA();
-        obj.MethodA();
-        ((Exercise1.IB)obj).MethodA();
-        Console.WriteLine(obj.MethodB(5));
+        /// <summary>
+        /// Runs the demo of <see cref="Exercise1"/>.
+        /// </summary>
+        public static void Run()
+        {
+            Exercise1.ClassA obj = new Exercise1.ClassA();
+            obj.MethodA();
+            ((Exercise1.IB)obj).MethodA();
+            Console.WriteLine(obj.MethodB(5));
+        }
     }
 }
--- trey-nash/Program.cs
+++ trey-nash/Program.cs
@@ -1,24 +1,27 @@
 ﻿using Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts;
-//TODO
-//namespace Fedotkin.Dotnet.TreyNash.General;
-
-/// <summary>
-/// Main entry point of the program.
-/// </summary>
-Console.Write("Enter chapter number (1-17): ");
-int chapterNo = 0;
-while (chapterNo == 0)
+class Program
 {
-    try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
-    catch { chapterNo = 0; }
-}
-// Select book chapter and run the demo
-switch (chapterNo)
-{
-    case 5:
-        Chapter5.Run();
-        break;
-    default:
-        Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
-        break;
+    /// <summary>
+    /// Main entry point of the program.
+    /// </summary>
+    static void Main()
+    {
+        Console.Write("Enter chapter number (1-17): ");
+        int chapterNo = 0;
+        while (chapterNo == 0)
+        {
+            try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
+            catch { chapterNo = 0; }
+        }
+        // Select book chapter and run the demo
+        switch (chapterNo)
+        {
+            case 5:
+                Chapter5.Run();
+                break;
+            default:
+                Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
+                break;
+        }
+    }
 }
