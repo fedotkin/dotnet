b002d0d487b344a82107b94dd31c5ee55a07c5ba
Alexei Fedotkin <110036665+fedotkin@users.noreply.github.com>
2022-12-27 17:56:39 +0300

    #25 Upgrade to .NET 7.0 (#31)
    * Change to namespaces new style
    
    * Changed to .NET 7.0 and used the new program style
    
    * Added description to TODO, fixed namespace
    
    * Update Program.cs
    
    Added number of issue TODO
    
    * Added Usings.cs

--- trey-nash/5-interfaces-and-contracts/Exercise1.cs
+++ trey-nash/5-interfaces-and-contracts/Exercise1.cs
@@ -1,49 +1,48 @@
-﻿namespace Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts
+﻿namespace Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts;
+
+/// <summary>
+/// The classes and interfaces of Exercise #1.
+/// </summary>
+internal class Exercise1
 {
     /// <summary>
-    /// The classes and interfaces of Exercise #1.
+    /// Initial interface which is rewritten as the abstract <see cref="ClassIA" /> class.
     /// </summary>
-    internal class Exercise1
+    public interface IA
     {
-        /// <summary>
-        /// Initial interface which is rewritten as the abstract <see cref="ClassIA" /> class.
-        /// </summary>
-        public interface IA
-        {
-            void MethodA();
-            bool PropA { get; }
-            int this[int index] { get; }
-            event EventHandler EventA;
-        }
-        /// <summary>
-        /// Abstract class instead of the <see cref="IA" /> interface (all signatures are abstract).
-        /// </summary>
-        public abstract class ClassIA
-        {
-            public abstract void MethodA();
-            public abstract bool PropA { get; }
-            public abstract int this[int index] { get; }
-            public abstract event EventHandler EventA;
-        }
-        /// <summary>
-        /// Interface that is used for inheritance in the new base abstract <see cref="ClassA"/> class.
-        /// </summary>
-        public interface IB
-        {
-            void MethodA();
-            int MethodB(int index);
-        }
-        /// <summary>
-        /// The new base abstract class that inherits the abstract <see cref="ClassIA"/> class and the <see cref="IB"/> interface.
-        /// </summary>
-        public class ClassA : ClassIA, IB
-        {
-            public override void MethodA() { Console.WriteLine("pip_1"); }
-            void IB.MethodA() { Console.WriteLine("pip_2"); }
-            public int MethodB(int index) { return index * 10; }
-            public override bool PropA { get { return true; } }
-            public override int this[int index] { get { return 1; } }
-            public override event EventHandler EventA;
-        }
+        void MethodA();
+        bool PropA { get; }
+        int this[int index] { get; }
+        event EventHandler EventA;
+    }
+    /// <summary>
+    /// Abstract class instead of the <see cref="IA" /> interface (all signatures are abstract).
+    /// </summary>
+    public abstract class ClassIA
+    {
+        public abstract void MethodA();
+        public abstract bool PropA { get; }
+        public abstract int this[int index] { get; }
+        public abstract event EventHandler EventA;
+    }
+    /// <summary>
+    /// Interface that is used for inheritance in the new base abstract <see cref="ClassA"/> class.
+    /// </summary>
+    public interface IB
+    {
+        void MethodA();
+        int MethodB(int index);
+    }
+    /// <summary>
+    /// The new base abstract class that inherits the abstract <see cref="ClassIA"/> class and the <see cref="IB"/> interface.
+    /// </summary>
+    public class ClassA : ClassIA, IB
+    {
+        public override void MethodA() { Console.WriteLine("pip_1"); }
+        void IB.MethodA() { Console.WriteLine("pip_2"); }
+        public int MethodB(int index) { return index * 10; }
+        public override bool PropA { get { return true; } }
+        public override int this[int index] { get { return 1; } }
+        public override event EventHandler EventA;
     }
 }
--- trey-nash/Program.cs
+++ trey-nash/Program.cs
@@ -1,27 +1,25 @@
 ﻿using Fedotkin.Dotnet.TreyNash.Ch5_InterfacesAndContracts;
-class Program
+
+// TODO: Issue #32. Define root namespace explicitly in the project.
+//namespace Fedotkin.Dotnet.TreyNash;
+
+/// <summary>
+/// Main entry point of the program.
+/// </summary>
+Console.Write("Enter chapter number (1-17): ");
+int chapterNo = 0;
+while (chapterNo == 0)
 {
-    /// <summary>
-    /// Main entry point of the program.
-    /// </summary>
-    static void Main()
-    {
-        Console.Write("Enter chapter number (1-17): ");
-        int chapterNo = 0;
-        while (chapterNo == 0)
-        {
-            try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
-            catch { chapterNo = 0; }
-        }
-        // Select book chapter and run the demo
-        switch (chapterNo)
-        {
-            case 5:
-                Chapter5.Run();
-                break;
-            default:
-                Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
-                break;
-        }
-    }
+    try { chapterNo = Convert.ToInt32(Console.ReadLine()); }
+    catch { chapterNo = 0; }
+}
+// Select book chapter and run the demo
+switch (chapterNo)
+{
+    case 5:
+        Chapter5.Run();
+        break;
+    default:
+        Console.WriteLine("Chapter {0}: Sorry, there are no exercises and no implemented solutions to demonstrate!", chapterNo);
+        break;
 }
