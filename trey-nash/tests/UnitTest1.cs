f97e305a28499a55d227756df9433ea085044ae2
Raman Maksimchuk <dotnet044@gmail.com>
2023-01-03 18:55:01 +0300

    Define root namespace as Fedotkin.Dotnet.TreyNash

--- trey-nash/tests/UnitTest1.cs
+++ trey-nash/tests/UnitTest1.cs
@@ -1,11 +1,10 @@
-namespace ChapterExercises.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace Fedotkin.Dotnet.TreyNash.ChapterExercises.Tests;
 
-        }
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
     }
 }
